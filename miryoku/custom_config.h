1e676b9b68f5582978ff51de5bbad59b84b16437
BryanNak <54912705+BryanNak@users.noreply.github.com>
2026-02-25 01:22:42 -1000

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,7 @@
 
 // Override base layer so F is tap F, hold Hyper (Ctrl+Shift+Gui+Alt).
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &f_hyper,          &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&kp Q,             &kp W,             U_MT(LG(LS(LA(LCTRL))), F), &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
