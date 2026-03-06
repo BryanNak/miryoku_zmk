cfd9d56013571ef6750820e2f26d7f600294d41d
BryanNak <54912705+BryanNak@users.noreply.github.com>
2026-03-05 21:59:14 -1000

    removed old Hyper "F" key and added urob's hrm

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,39 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// Override base layer so F is tap F, hold Hyper (Ctrl+Shift+Gui+Alt).
-#define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             U_MT(LG(LS(LA(LCTRL))), F), &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+// 1. Define Key Positions for a 36-key Corne (3x5+3)
+// These indices match the Miryoku 36-key layout mapping
+#define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24
+#define KEYS_R 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29
+#define THUMBS 30 31 32 33 34 35
+
+// 2. Inject urob's "Timeless" behaviors into the top level
+#define MIRYOKU_KLUDGE_TAP_P \
+    hml: homrow_mods_left { \
+        compatible = "zmk,behavior-hold-tap"; \
+        #binding-cells = <2>; \
+        flavor = "balanced"; \
+        tapping-term-ms = <280>; \
+        quick-tap-ms = <175>; \
+        require-prior-idle-ms = <150>; \
+        bindings = <&kp>, <&kp>; \
+        hold-trigger-key-positions = <KEYS_R THUMBS>; \
+        hold-trigger-on-release; \
+    }; \
+    hmr: homrow_mods_right { \
+        compatible = "zmk,behavior-hold-tap"; \
+        #binding-cells = <2>; \
+        flavor = "balanced"; \
+        tapping-term-ms = <280>; \
+        quick-tap-ms = <175>; \
+        require-prior-idle-ms = <150>; \
+        bindings = <&kp>, <&kp>; \
+        hold-trigger-key-positions = <KEYS_L THUMBS>; \
+        hold-trigger-on-release; \
+    };
+
+// 3. Redefine the MT macro to use our new behaviors
+#undef U_MT
+#define U_MT(MOD, TAP) &u_mt MOD TAP
+#define u_mt(MOD, TAP) \
+    &hml MOD TAP | &hmr MOD TAP
