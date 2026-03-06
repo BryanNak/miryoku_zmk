866cd492858dbda83b6b7ec633915254e61951ef
BryanNak <54912705+BryanNak@users.noreply.github.com>
2026-03-05 23:15:16 -1000

    work pls

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,38 +1,42 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 //
-// 1. Define Key Positions for 36-key Corne
+// 1. Key Positions for 36-key Corne
 #define KEYS_L 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24
 #define KEYS_R 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29
 #define THUMBS 30 31 32 33 34 35
 
-// 2. Inject urob's "Timeless" behaviors
-#define MIRYOKU_KLUDGE_BEHAVIORS_P \
-    hml: homrow_mods_left { \
-        compatible = "zmk,behavior-hold-tap"; \
-        #binding-cells = <2>; \
-        flavor = "balanced"; \
-        tapping-term-ms = <280>; \
-        quick-tap-ms = <175>; \
-        require-prior-idle-ms = <150>; \
-        bindings = <&kp>, <&kp>; \
-        hold-trigger-key-positions = <KEYS_R THUMBS>; \
-        hold-trigger-on-release; \
-    }; \
-    hmr: homrow_mods_right { \
-        compatible = "zmk,behavior-hold-tap"; \
-        #binding-cells = <2>; \
-        flavor = "balanced"; \
-        tapping-term-ms = <280>; \
-        quick-tap-ms = <175>; \
-        require-prior-idle-ms = <150>; \
-        bindings = <&kp>, <&kp>; \
-        hold-trigger-key-positions = <KEYS_L THUMBS>; \
-        hold-trigger-on-release; \
+// 2. Define urob's behaviors in the global behaviors node
+// We use MIRYOKU_KLUDGE_TOP_LEVEL to inject these at the root of the dtsi
+#define MIRYOKU_KLUDGE_TOP_LEVEL \
+    / { \
+        behaviors { \
+            hml: homrow_mods_left { \
+                compatible = "zmk,behavior-hold-tap"; \
+                #binding-cells = <2>; \
+                flavor = "balanced"; \
+                tapping-term-ms = <280>; \
+                quick-tap-ms = <175>; \
+                require-prior-idle-ms = <150>; \
+                bindings = <&kp>, <&kp>; \
+                hold-trigger-key-positions = <KEYS_R THUMBS>; \
+                hold-trigger-on-release; \
+            }; \
+            hmr: homrow_mods_right { \
+                compatible = "zmk,behavior-hold-tap"; \
+                #binding-cells = <2>; \
+                flavor = "balanced"; \
+                tapping-term-ms = <280>; \
+                quick-tap-ms = <175>; \
+                require-prior-idle-ms = <150>; \
+                bindings = <&kp>, <&kp>; \
+                hold-trigger-key-positions = <KEYS_L THUMBS>; \
+                hold-trigger-on-release; \
+            }; \
+        }; \
     };
 
-// 3. Overwrite the Base Layer to use the new behaviors
-// This bypasses the U_MT macro entirely to prevent the "2 arguments" error
+// 3. Manually define the Base Layer to use the labels defined above
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &hml LGUI A,       &hml LALT S,       &hml LCTRL D,      &hml LSHFT F,      &kp G,             &kp H,             &hmr LSHFT J,      &hmr LCTRL K,      &hmr LALT L,       &hmr LGUI SEMI,    \
